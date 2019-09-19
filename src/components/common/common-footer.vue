b8af45013b539bea82019775f7ac1a3799c8c8eb
Phoenix <123456>
2019-09-19 13:45:00 +0800

    重新整理结构

---
+++ src/components/common/common-footer.vue
@@ -0,0 +1,477 @@
+<template>
+    <el-row class="index-footer">
+        <el-col class="block" :xs="0" :sm="0" :md="1" :lg="2" :xl="3">&nbsp;</el-col>
+        <el-col class="content" :xs="24" :sm="24" :md="22" :lg="20" :xl="18" style="background: white">
+            <el-row class="our-link">
+                <el-col class="hidden-sm-and-down our-link-list logo" :md="2" :lg="2" :xl="2">
+                    <a href=""><img src="../../assets/images/index/klientboost-logo-blue.png"></a>
+                </el-col>
+                <el-col :xs="9" :sm="1" :md="1" :lg="1" :xl="1">&nbsp;</el-col>
+
+                <el-col class="our-link-list" :xs="12" :sm="3" :md="3" :lg="3" :xl="3">
+                    <h3>AGENCY</h3>
+                    <ul class="our-link-ul">
+                        <li>
+                            <a href="">What We Do</a>
+                        </li>
+                        <li>
+                            <a href="">Case Studies</a>
+                        </li>
+                        <li>
+                            <a href="">Blog</a>
+                        </li>
+                        <li>
+                            <a href="">Careers</a>
+                        </li>
+                        <li>
+                            <a href="">Contact Us</a>
+                        </li>
+                    </ul>
+                </el-col>
+                <el-col :xs="9" class="hidden-sm-and-up">&nbsp;</el-col>
+
+                <el-col class="our-link-list" :xs="12" :sm="7" :md="5" :lg="6" :xl="6">
+                    <h3>PAY-PER-CLICK</h3>
+                    <ul class="our-link-ul">
+                        <li>
+                            <a href="">PPC Management</a>
+                        </li>
+                        <li>
+                            <a href="">PPC Agency</a>
+                        </li>
+                        <li>
+                            <a href="">AdWords Management Company</a>
+                        </li>
+                        <li>
+                            <a href="">Facebook Ads Agency</a>
+                        </li>
+                        <li>
+                            <a href="">SEM Agency</a>
+                        </li>
+                    </ul>
+                </el-col>
+                <el-col :xs="9" class="hidden-sm-and-up">&nbsp;</el-col>
+
+                <el-col class="our-link-list" :xs="12" :sm="7" :md="6" :lg="6" :xl="6">
+                    <h3>CONVERSION</h3>
+                    <ul class="our-link-ul">
+                        <li>
+                            <a href="">Conversion Rate Optimization</a>
+                        </li>
+                        <li>
+                            <a href="">Conversion Agency</a>
+                        </li>
+                        <li>
+                            <a href="">Conversion Optimization Services</a>
+                        </li>
+                        <li>
+                            <a href="">Conversion Optimization Agency</a>
+                        </li>
+                        <li>
+                            <a href="">Conversion Optimization Specialist</a>
+                        </li>
+                    </ul>
+                </el-col>
+                <el-col :xs="9" class="hidden-sm-and-up">&nbsp;</el-col>
+
+                <el-col class="our-link-list" :xs="12" :sm="6" :md="5" :lg="5" :xl="5">
+                    <h3>DESIGN</h3>
+                    <ul class="our-link-ul">
+                        <li>
+                            <a href="">Landing Page Design</a>
+                        </li>
+                        <li>
+                            <a href="">Landing Page Agency</a>
+                        </li>
+                        <li>
+                            <a href="">Custom Landing Page Design</a>
+                        </li>
+                        <li>
+                            <a href="">Landing Page Company</a>
+                        </li>
+                        <li>
+                            <a href="">Landing Page Design Service</a>
+                        </li>
+                    </ul>
+                </el-col>
+            </el-row>
+            <el-row class="hr-line">
+                <el-col :xs="0" :sm="2" :md="3" :lg="0" :xl="4" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="20" :md="18" :lg="24" :xl="16" class="hr">&nbsp;</el-col>
+            </el-row>
+            <el-row>
+                <el-col :xs="0" :sm="4" :md="4" :lg="4" :xl="7" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="10">
+                    <el-row class="contact-us">
+                        <el-col :xs="7" :sm="7" :md="7" :lg="7" :xl="7">&nbsp;</el-col>
+                        <el-col :xs="13" :sm="14" :md="12" :lg="10" :xl="10">
+                            <ul>
+                                <li class="contact-us-item">
+                                    <img src="../../assets/images/index/orange-county-office-badge-2x.png" alt="">
+                                    <div class="contact-us-item-content">
+                                        <h3>ORANGE COUNTY</h3>
+                                        <p>
+                                            2787 Bristol Street,<br/>
+                                            Costa Mesa, CA 92626
+                                        </p>
+                                        <a href="">(877) 501-3447</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </el-col>
+                    </el-row>
+                </el-col>
+            </el-row>
+            <el-row class="hr-line">
+                <el-col :xs="0" :sm="2" :md="2" :lg="4" :xl="4" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="20" :md="20" :lg="16" :xl="16" class="hr">&nbsp;</el-col>
+            </el-row>
+            <el-row>
+                <el-col :xs="0" :sm="4" :md="4" :lg="4" :xl="7" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="10">
+                    <el-row class="question">
+                        <!--                <div class="question">-->
+                        <!--                       <el-col :xs="0" :sm="1" :md="2" :lg="5" :xl="0">&nbsp;</el-col>-->
+                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+                            <div class="question-content">
+                                <span class="el-icon-question"></span>
+                                <div class="question-text-content">
+                                    <h3>Have a question?</h3>
+                                    <p>WE DON'T BITE.</p>
+                                </div>
+                            </div>
+                        </el-col>
+                        <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+                            <div class="question-content">
+                                <div class="question-button">
+                                    <span>hello@klientboost.com</span>
+                                </div>
+                            </div>
+                        </el-col>
+                    </el-row>
+                </el-col>
+            </el-row>
+            <el-row class="follow-logo">
+                <el-col :xs="0" :sm="4" :md="4" :lg="4" :xl="7" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="10">
+                    <h3>Currently Accepting Followers</h3>
+                    <ul>
+                        <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                        <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href=""><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                        <li><a href=""><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                </el-col>
+            </el-row>
+            <el-row class="footer-img">
+                <el-col :xs="0" :sm="4" :md="4" :lg="4" :xl="4" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="16">
+                    <a href="" class="img-badge">
+                        <img src="../../assets/images/index/partner-badge-google.png" alt="">
+                    </a>
+                    <a href="" class="img-badge">
+                        <img src="../../assets/images/index/MSA-Partner-badge-grey.png" alt="">
+                    </a>
+                </el-col>
+            </el-row>
+            <el-row class="footer-summary">
+                <el-col :xs="0" :sm="4" :md="4" :lg="4" :xl="4" class="block">&nbsp;</el-col>
+                <el-col :xs="24" :sm="16" :md="16" :lg="16" :xl="16">
+                    <div class="footer-copyright">©‎ KlientBoost 2019</div>
+                    <div class="footer-terms">
+                        <a href="">Terms of Service</a>
+                        <span>|</span>
+                        <a href="">Privacy Policy</a>
+                    </div>
+                </el-col>
+            </el-row>
+            <br/>
+            <br/>
+            <br/>
+        </el-col>
+        <Footer v-bind:class="{'isIndex':isNowPage}"></Footer>
+    </el-row>
+</template>
+
+<script>
+
+  import Footer from '@/components/footerview'
+  import 'element-ui/lib/theme-chalk/display.css'
+
+  export default {
+    name: 'common-footer',
+    components: {
+      Footer
+    },
+    data () {
+      return {
+        isNowPage: true,
+        isFixed: false,
+        offsetTop: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    .index-footer {
+        padding: 40px 16px;
+        /*background: #E0E0E0;*/
+        width: 100%;
+    }
+
+    .index-footer .content .logo {
+        width: 120px;
+    }
+
+    .index-footer .content .our-link .our-link-list {
+        /*flex: 0 0 auto;*/
+        /*flex-direction:row;*/
+        /*flex-wrap: wrap;*/
+        box-sizing: border-box;
+        text-align: left;
+        margin-bottom: calc((40px - 2.5rem));
+    }
+
+    .index-footer .content .our-link-ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    .index-footer .content .our-link .our-link-list img {
+        width: 100%;
+        max-width: 100%;
+    }
+
+    .index-footer .content .our-link .our-link-list h3 {
+        font-size: 15px;
+        font-weight: 700;
+        line-height: 100%;
+        text-transform: uppercase;
+        color: #667482;
+        letter-spacing: .022em;
+        margin-bottom: calc(1rem);
+    }
+
+    .index-footer .content .our-link .our-link-list li {
+        margin-bottom: 14px;
+    }
+
+    .index-footer .content .our-link .our-link-list li a {
+        color: #667482;
+        text-decoration: none;
+        font-size: 12px;
+        line-height: 100%;
+        font-family: Lato, Helvetica Neue, Helvetica;
+        outline: 0 !important;
+        transition: all .1s ease;
+    }
+
+    .index-footer .content .our-link .our-link-list li a:hover {
+        color: #6adaf7;
+    }
+
+    .index-footer .hr {
+        margin: 40px auto 0;
+        padding-bottom: 40px;
+        border-top: 1px solid #ced5dd;
+    }
+
+    .index-footer .contact-us {
+        display: block;
+        text-align: left;
+    }
+
+    .index-footer .contact-us .contact-us-item img {
+        width: 80px;
+        height: 80px;
+        margin-right: 16px;
+        float: left;
+    }
+
+    .index-footer .contact-us .contact-us-item h3 {
+        margin-bottom: 6px;
+        font-size: 15px;
+        font-weight: 700;
+        line-height: 100%;
+        text-transform: uppercase;
+        color: #667482;
+        letter-spacing: .022em;
+    }
+
+    .index-footer .contact-us .contact-us-item p {
+        margin: 0 0 10px;
+    }
+
+    .index-footer .contact-us .contact-us-item a {
+        font-weight: 700;
+        color: #6adaf7;
+        text-decoration: none;
+        outline: 0 !important;
+        transition: all .1s ease;
+    }
+
+    .index-footer .contact-us .contact-us-item a:hover {
+        color: #5dc0d9;
+    }
+
+    .index-footer .question-content {
+
+    }
+
+    .index-footer .question {
+        padding: 30px 40px 30px 40px;
+        /*display: inline-flex;*/
+        margin-bottom: 40px;
+        border: 1px solid rgba(0, 0, 0, .1);
+        border-radius: 5px;
+    }
+
+    .index-footer .el-icon-question,
+    .index-footer .question-text-content {
+        display: inline-block;
+        text-align: left;
+        color: #667482;
+    }
+
+    .index-footer .question-content:first-child {
+        padding-bottom: 10px;
+    }
+
+    .index-footer .question-content:last-child {
+        padding-top: 10px;
+    }
+
+    .index-footer .question-content .el-icon-question {
+        color: #6ADAF7;
+        font-size: 46px;
+        margin-right: 15px;
+    }
+
+    .index-footer .question-text-content h3 {
+        font-weight: 700;
+        font-size: 20px;
+        margin-bottom: 6px;
+        line-height: 100%;
+    }
+
+    .index-footer .question-text-content p {
+        text-transform: uppercase;
+        color: #667482;
+        font-size: 14px;
+        margin-bottom: 0;
+        font-weight: 500;
+        letter-spacing: .03em;
+    }
+
+    .index-footer .question-content .question-button span {
+        color: #ff5056;
+        border: 2px solid #ff5056;
+        background: none;
+        outline: 0 !important;
+        border-radius: 4px;
+        line-height: 100%;
+        display: inline-block;
+        text-transform: uppercase;
+        font-weight: 700;
+        padding: 12px 16px;
+        cursor: pointer;
+    }
+
+    .index-footer .question-content .question-button span:hover {
+        background: #ff5056;
+        color: #fff;
+    }
+
+    .index-footer .follow-logo {
+        margin-bottom: 24px;
+    }
+
+    .index-footer .follow-logo h3 {
+        font-size: 15px;
+        font-weight: 700;
+        line-height: 100%;
+        text-transform: uppercase;
+        color: #667482;
+        font-family: inherit;
+        letter-spacing: .022em;
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+    .index-footer .follow-logo ul {
+        list-style: none;
+        margin-bottom: 15px;
+    }
+
+    .index-footer .follow-logo li {
+        display: inline-block;
+        margin: 0 1rem;
+    }
+
+    .index-footer .follow-logo li:first-child {
+        margin-left: 0px;
+    }
+
+    .index-footer .follow-logo li:last-child {
+        margin-right: 0px;
+    }
+
+    .index-footer .follow-logo li a {
+        color: #fff;
+        background: #6adaf7;
+        border-radius: 999px;
+        display: block;
+        height: 40px;
+        width: 40px;
+        line-height: 40px;
+        font-size: 20px;
+        text-align: center;
+    }
+
+    .index-footer .follow-logo li a:hover {
+        color: #fff;
+        background: #ff5056;
+    }
+
+    .index-footer .footer-img {
+        line-height: 1.42857;
+        display: block;
+        margin-bottom: 24px;
+    }
+
+    .index-footer .img-badge {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    .index-footer .img-badge img {
+        max-width: 100%;
+        height: 48px;
+        width: auto;
+    }
+
+    .index-footer .footer-summary {
+        color: #667482;
+        font-family: Lato, Helvetica Neue, Helvetica;
+        line-height: 1.42857;
+    }
+
+    .index-footer .footer-summary .footer-copyright {
+        margin-bottom: 8px;
+    }
+
+    .index-footer .footer-summary .footer-terms a {
+        color: #667482;
+    }
+
+    .index-footer .footer-summary .footer-terms a:hover {
+        color: #6adaf7;;
+    }
+
+    .index-footer .footer-summary .footer-terms span {
+        margin: 0 10px;
+    }
+</style>
